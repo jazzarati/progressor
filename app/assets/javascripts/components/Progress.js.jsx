60dea7f856646987180a5f33de7e1d7cf0eeaccf
Jarrod <you@example.com>
2016-05-20 12:06:49 +1000

    Fix no completed stuff for progress bar

--- app/assets/javascripts/components/Progress.js.jsx
+++ app/assets/javascripts/components/Progress.js.jsx
@@ -1,15 +1,19 @@
 class Progress extends React.Component {
   points() {
     let challenges = [...this.props.store.project.monsters, ...this.props.store.project.quests]
-    return challenges.filter( challenge => { return challenge.completed_at !== null } )
-              .map( challenge => { return challenge.points } )
-              .reduce( (previousValue, currentValue) => { return previousValue + currentValue } )
+    let completed = challenges.filter( challenge => { return challenge.completed_at !== null } )
+    if (completed.length === 0) {
+      return 0
+    } else {
+      return completed.map( challenge => { return challenge.points } )
+                      .reduce( (previousValue, currentValue) => { return previousValue + currentValue } )
+    }
   }
 
   render() {
     return (
       <div className="progress">
-        <div className="progress-bar progress-bar-info" role="progressbar" aria-valuenow={this.points()} aria-valuemin="0" aria-valuemax="1000" style={ { width: '20%' } }>
+        <div className="progress-bar progress-bar-info" role="progressbar" aria-valuenow={this.points()} aria-valuemin="0" aria-valuemax="1000" style={ { width: this.points() + '%' } }>
           {this.points()}
           <span className="sr-only">{this.points()} Complete</span>
         </div>
